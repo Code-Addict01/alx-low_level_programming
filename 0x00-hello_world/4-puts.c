b987d6ce610fc7e679616c70883a96cd21a822e8
Code-Addict01 <davidckihoto@gmail.com>
2023-10-28 16:07:58 -0700

    The funtion puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
