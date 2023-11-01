8af8c68cacbc12429b8fb971c17149a9d0e1afe0
Code-Addict01 <davidckihoto@gmail.com>
2023-10-31 19:23:00 -0700

    Print single chars in loop to from string

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
