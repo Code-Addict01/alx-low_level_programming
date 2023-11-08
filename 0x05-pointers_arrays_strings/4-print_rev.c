0e995ca65787d0855ccac803598c19ef43bce089
Code-Addict01 <davidckihoto@gmail.com>
2023-11-07 18:39:38 -0800

    Reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,16 @@ void print_rev(char *s)
 {
 	int index, len;
 
-	for (index = 0; s[index] != '\0'; index++)
+	index = 0;
+	while (s[index] != '\0')
 	{
-		len = index;
+		index++;
 	}
+
+	len = index;
 
 	while (len >= 0)
 	{
-		if (s == NULL || *s == '\0')
-			return;
 		_putchar(*(s + len));
 		len--;
 	}
