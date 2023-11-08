4a1256f01262b32f64c50a257a4bc08f7310c850
Code-Addict01 <davidckihoto@gmail.com>
2023-11-07 17:49:44 -0800

    Know its length first

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to every other character in the string
+ */
+void print_rev(char *s)
+{
+	int index, len;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		len = index;
+	}
+
+	while (s[len] != '\0')
+	{
+		_putchar(*(s + len));
+		len--;
+	}
+	_putchar('\n');
+}
