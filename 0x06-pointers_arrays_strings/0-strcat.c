a3457abd55861e5d7dbe2b71055a05bf3e2950ff
Code-Addict01 <davidckihoto@gmail.com>
2023-12-05 17:01:22 -0800

    Task one done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: where src will be appended
+ * @src: the string to append
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	return (dest);
+}
