0620bd808ed28cb459d812aa9f3ae887f87b073a
Code-Addict01 <davidckihoto@gmail.com>
2024-02-29 13:22:53 -0800

    0 no longer greater than 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
+	largest = a;
+
+	if (b > largest)
 	{
 		largest = b;
 	}
-	else if ((c > a && a > b) || (c > b && b > a))
+	if (c > largest)
 	{
 		largest = c;
 	}
+
 
 	return (largest);
 }
