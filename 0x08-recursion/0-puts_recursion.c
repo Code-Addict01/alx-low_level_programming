b1ebe23b4a6d1d3a90ecaad45dfd5e6368530f0e
Code-Addict01 <davidckihoto@gmail.com>
2024-03-07 05:09:42 -0800

    puts recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints string followed by new line recursively
+ * @s: pointer to string to print
+ */
+void _puts_recursion(char *s)
+{
+	int index;
+
+	index = 0;
+	if (s[index] != '\0')
+	{
+		_putchar(s[index]);
+	}
+	_puts_recursion(s + 1);
+
+	_putchar('\n');
+}
