60eb6b2f849902ca926fd9c23557974859455b6e
Code-Addict01 <davidckihoto@gmail.com>
2024-05-09 07:50:08 -0700

    Task 1 for preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef H_0_OBJECT_LIKE_MACRO_H
+#define H_0_OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif /*H_0_OBJECT_LIKE_MACRO_H*/
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,7 @@
+#0x0D-preprocessor
+---
+
+This directory contains tasks on prepocessor concepts
+
+1. Include guard
+2. Macros
