62ec894f02a71343cfe680083985bf00d54d7870
Code-Addict01 <davidckihoto@gmail.com>
2024-06-24 03:52:18 -0700

    realloc function with malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,12 +15,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 	{
-		return (NULL);
+		return (ptr);
 	}
 
 	if (!ptr)
 	{
-		return (malloc(new_size));
+		ptr = (malloc(new_size));
+		return (ptr);
 	}
 
 	if (new_size == 0 && ptr)
