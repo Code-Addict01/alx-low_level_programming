97696b4a991f3f7f414f0905324211510c77a89d
Code-Addict01 <davidckihoto@gmail.com>
2024-07-02 07:49:04 -0700

    1st encounter with array of integers, it was awesome

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,6 +20,13 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+
 	f = get_op_func(argv[2]);
 
 	if (f == NULL)
@@ -28,10 +35,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if (f)
-	{
-		printf("%d\n", f(num1, num2));
-	}
+	printf("%d\n", f(num1, num2));
 
 	return (0);
 }
