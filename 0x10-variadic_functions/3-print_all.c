dc02a2766022a48793f5b6aa3c5524fd2844b83f
Code-Addict01 <davidckihoto@gmail.com>
2024-07-11 13:12:23 -0700

    Function that prints its variadic arguments of different types

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,82 +1,51 @@
 #include "variadic_functions.h"
 
 /**
- * print_char - prints char type argument
- * @ap: va_list variable
- */
-void print_char(va_list ap)
-{
-	printf("%c", va_arg(ap, int));
-}
-
-/**
- * print_int - print integer type argument
- * @ap: va_list variable
- */
-void print_int(va_list ap)
-{
-	printf("%d", va_arg(ap, int));
-}
-
-/**
- * print_float - prints float type argument
- * @ap: va_list variable
- */
-void print_float(va_list ap)
-{
-	printf("%f", va_arg(ap, double));
-}
-
-/**
- * print_str - prints string type argument
- * @ap: va_list variable
- */
-void print_str(va_list ap)
-{
-	char *str = va_arg(ap, char *);
-
-	if (str)
-		printf("%s", str);
-	else
-		printf("(nil)");
-}
-
-/**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: no return.
  */
 void print_all(const char * const format, ...)
 {
-	va_list ap;
-	unsigned int i, j, c = 0, size;
-	format_args t_fn[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_str},
-		{'\0', NULL}
-	};
-	size = sizeof(t_fn) / sizeof(t_fn[0]);
+        va_list valist;
+        unsigned int i = 0, j, c = 0;
+        char *str;
+        const char t_arg[] = "cifs";
 
-	va_start(ap, format);
-	i = 0;
-	while (format && format[i])
-	{
-		j = 0;
-		while (j < size)
-		{
-			if (format[i] == t_fn[j].specifier)
-			{
-				if (c)
-					printf(", ");
-				t_fn[j].func_ptr(ap);
-				c = 1;
-				break;
-			}
-			j++;
-		}
-		i++;
-	}
-	printf("\n");
-	va_end(ap);
+        va_start(valist, format);
+        while (format && format[i])
+        {
+                j = 0;
+                while (t_arg[j])
+                {
+                        if (format[i] == t_arg[j] && c)
+                        {
+                                printf(", ");
+                                break;
+                        } j++;
+                }
+                switch (format[i])
+                {
+                case 'c':
+                        printf("%c", va_arg(valist, int)), c = 1;
+                        break;
+                case 'i':
+                        printf("%d", va_arg(valist, int)), c = 1;
+                        break;
+                case 'f':
+                        printf("%f", va_arg(valist, double)), c = 1;
+                        break;
+                case 's':
+                        str = va_arg(valist, char *), c = 1;
+                        if (!str)
+                        {
+                                printf("(nil)");
+                                break;
+                        }
+                        printf("%s", str);
+                        break;
+                } i++;
+        }
+        printf("\n"), va_end(valist);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,16 +9,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-/**
- * struct format_function_t - holds argument format specifier &
- * pointer to functions for printing certain formats
- * @specifier: format specifier
- * @func_ptr: pointer to function printing a specific type of argument
- */
-typedef struct format_function_t
-{
-	char specifier;
-	void (*func_ptr)(va_list);
-} format_args;
-
 #endif /*VARIADIC_FUNCTIONS_H*/
