c995a16f581aa3570136a8fb86793e1193113b27
Code-Addict01 <davidckihoto@gmail.com>
2024-07-11 12:42:22 -0700

    Function prints variadic arguments of different types

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,80 @@
+#include "variadic_functions.h"
+
+/**
+ * print_char - prints char type argument
+ * @ap: va_list variable
+ */
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_int - print integer type argument
+ * @ap: va_list variable
+ */
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float - prints float type argument
+ * @ap: va_list variable
+ */
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_str - prints string type argument
+ * @ap: va_list variable
+ */
+void print_str(va_list ap)
+{
+	char *str = va_arg(ap, char *);
+
+	if (str)
+		printf("%s", str);
+	else
+		printf("(nil)");
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	unsigned int i, j, c = 0;
+	format_args t_fn[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_str}
+	};
+
+	va_start(ap, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (t_fn[j].func_ptr)
+		{
+			if (format[i] == t_fn[j].specifier)
+			{
+				if (c)
+					printf(", ");
+				t_fn[j].func_ptr(ap);
+				c = 1;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
