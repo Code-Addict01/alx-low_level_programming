9a78474d7cd737f1322cae8c056258e8bbf5084a
Code-Addict01 <davidckihoto@gmail.com>
2024-07-22 08:31:25 -0700

    Add new node in a linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (!new_node)
-		free(new_node);
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (!new_node->str)
