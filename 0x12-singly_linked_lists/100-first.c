5691e7251c16a8732084655eb98fc1cf9fb5c8e4
Code-Addict01 <davidckihoto@gmail.com>
2024-07-23 04:59:10 -0700

    Function execute before main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@ void print_b4_main(void) __attribute__((constructor));
 /**
  * print_b4_main - prints a sentence before the main function is executed
  */
-void print_b4_main(void)
+void __attribute__((constructor)) print_b4_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
