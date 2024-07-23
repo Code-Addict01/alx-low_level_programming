54e70778a848afd88bded0351d4377f5bb1823de
Code-Addict01 <davidckihoto@gmail.com>
2024-07-23 04:38:17 -0700

    Function execute before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * printf_b4_main - prints a sentence before the main function is executed
+ */
+void print_b4_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
