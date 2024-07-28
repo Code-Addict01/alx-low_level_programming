b9d016d07d1e3bd3cad9191daa65e7e82f915dd2
Code-Addict01 <davidckihoto@gmail.com>
2024-07-28 04:41:50 +0300

    Task  14 done!

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,15 +9,17 @@
 int main(void)
 {
 	int i, sum3, sum5, sum15, t_sum;
-	sum3 = sum5 = sum15 = 0;
+	sum3 = 0, sum5 = 0, sum15 = 0;
 
 	for (i = 1; i <= 1024; i++)
 	{
 		if (i % 3)
 			sum3 += i;
-		else if (i % 5)
+
+		if (i % 5)
 			sum5 += i;
-		else if (i % 15)
+
+		if (i % 15)
 			sum15 += i;
 	}
 	t_sum = sum3 + sum5 - sum15;
